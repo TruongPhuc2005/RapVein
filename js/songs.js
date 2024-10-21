33a07ba49b3d0b94d51e8b5f3a03f7c57c6970f4
Nguyen Duong Truong Phuc <nguyenduongtruongphuc@gmail.com>
2024-10-21 14:12:09 -0400

    Create songs.js

---
+++ js/songs.js
@@ -0,0 +1,613 @@
+/**
+ * songs.js
+ *
+ * The app's songs
+ */
+
+window.songs = [
+  // Central Cee's songs
+  {
+    songId: "SID-101",
+    artistId: "AID-1111",
+    title: "BAND4BAND",
+    album: "BAND4BAND",
+    year: "2024",
+    duration: 140,
+    url: "https://open.spotify.com/track/0oZ1hEmIpbO3PzREe2HZeL?si=473a66c098754d14",
+    explicit: false
+  },
+  {
+    songId: "SID-102",
+    artistId: "AID-1111",
+    title: "Sprinter",
+    album: "Bravo: The Hits 2023",
+    year: "2023",
+    duration: 229,
+    url: "https://open.spotify.com/track/2FDTHlrBguDzQkp7PVj16Q?si=edba79c516594718",
+    explicit: false
+  },
+  {
+    songId: "SID-103",
+    artistId: "AID-1111",
+    title: "Doja",
+    album: "Doja",
+    year: "2022",
+    duration: 157,
+    url: "https://open.spotify.com/track/3LtpKP5abr2qqjunvjlX5i?si=16ff547de9d34bf1",
+    explicit: false
+  },
+  {
+    songId: "SID-104",
+    artistId: "AID-1111",
+    title: "Wave",
+    album: "Lungu Boy",
+    year: "2024",
+    duration: 205,
+    url: "https://open.spotify.com/track/7i4m8FybgjufzfvhO7gAKf?si=074ea16bcb6c40ab",
+    explicit: false
+  },
+  {
+    songId: "SID-105",
+    artistId: "AID-1111",
+    title: "Did It First",
+    album: "Y2K!",
+    year: "2024",
+    duration: 118,
+    url: "https://open.spotify.com/track/57lsYy0OZLNb5OQ7fEa3Hs?si=e3f20f1221b14c0a",
+    explicit: false
+  },
+  {
+    songId: "SID-106",
+    artistId: "AID-1111",
+    title: "Loading",
+    album: "Loading",
+    year: "2021",
+    duration: 173,
+    url: "https://open.spotify.com/track/4vzJrkfHbCGaodPA5RY5BV?si=e92be7e194394ec9",
+    explicit: false
+  },
+  {
+    songId: "SID-107",
+    artistId: "AID-1111",
+    title: "LET GO",
+    album: "LET GO",
+    year: "2022",
+    duration: 174,
+    url: "https://open.spotify.com/track/3zkyus0njMCL6phZmNNEeN?si=d622052039ee4707",
+    explicit: false
+  },
+  {
+    songId: "SID-108",
+    artistId: "AID-1111",
+    title: "TOO MUCH",
+    album: "The First Time",
+    year: "2023",
+    duration: 203,
+    url: "https://open.spotify.com/track/100fbGocyqbBPQkTDchi2g?si=256194b8e0a74418",
+    explicit: true
+  },
+  {
+    songId: "SID-109",
+    artistId: "AID-1111",
+    title: "Lil Bro",
+    album: "23",
+    year: "2022",
+    duration: 189,
+    url: "https://open.spotify.com/track/4tok5E9QajOVkHO64XQsk9?si=b0ad739d291b42c7",
+    explicit: false
+  },
+  {
+    songId: "SID-110",
+    artistId: "AID-1111",
+    title: "Overseas",
+    album: "Home Alone 2",
+    year: "2021",
+    duration: 222,
+    url: "https://open.spotify.com/track/337kcYVjYXdLBItCw9ry3b?si=3046518492934e80",
+    explicit: false
+  },
+  {
+    songId: "SID-111",
+    artistId: "AID-1111",
+    title: "6 For 6",
+    album: "6 For 6",
+    year: "2021",
+    duration: 148,
+    url: "https://open.spotify.com/track/1tLV0GD0WXQ1Z8BKOQscti?si=17275d42c84a4e35",
+    explicit: false
+  },
+  {
+    songId: "SID-112",
+    artistId: "AID-1111",
+    title: "Khabib",
+    album: "23",
+    year: "2022",
+    duration: 201,
+    url: "https://open.spotify.com/track/2ZVSjes0VFYg9uJu4olsi4?si=37617bda1a934cba",
+    explicit: false
+  },
+  {
+    songId: "SID-113",
+    artistId: "AID-1111",
+    title: "One Up",
+    album: "No More Leaks",
+    year: "2022",
+    duration: 160,
+    url: "https://open.spotify.com/track/4FAQjmX7e7JJaFE363uj86?si=cfe2b8519fe14e19",
+    explicit: false
+  },
+  {
+    songId: "SID-114",
+    artistId: "AID-1111",
+    title: "Me & You",
+    album: "Me & You",
+    year: "2023",
+    duration: 187,
+    url: "https://open.spotify.com/track/12Us8e6NFesdkXKf4StU1I?si=277ed4306b934f64",
+    explicit: false
+  },
+  {
+    songId: "SID-115",
+    artistId: "AID-1111",
+    title: "Straight Back to It",
+    album: "23",
+    year: "2022",
+    duration: 196,
+    url: "https://open.spotify.com/track/709MdyUol3Qfgcowmd7KV4?si=36330d1b39d947b8",
+    explicit: false
+  },
+  {
+    songId: "SID-116",
+    artistId: "AID-1111",
+    title: "gen z luv",
+    album: "gen z luv",
+    year: "2024",
+    duration: 153,
+    url: "https://open.spotify.com/track/13hJUmR1UpCUzyHjotiImK?si=9f3e63cebc0b4392",
+    explicit: false
+  },
+  {
+    songId: "SID-117",
+    artistId: "AID-1111",
+    title: "Trojan Horse",
+    album: "Split Decision",
+    year: "2023",
+    duration: 248,
+    url: "https://open.spotify.com/track/2HALQBSAvpw1oCzL5QsrT2?si=90b4cb3e45374587",
+    explicit: true
+  },
+  {
+    songId: "SID-118",
+    artistId: "AID-1111",
+    title: "I Will",
+    album: "I Will",
+    year: "2024",
+    duration: 149,
+    url: "https://open.spotify.com/track/27sKtH0PZpASkN8H3ZJOQf?si=2d521eae77d3497c",
+    explicit: false
+  },
+  {
+    songId: "SID-119",
+    artistId: "AID-1111",
+    title: "Ungrateful",
+    album: "23",
+    year: "2022",
+    duration: 173,
+    url: "https://open.spotify.com/track/1fBaPpiFIepmaMkJqHcPBh?si=f7b67df6ddf94e39",
+    explicit: false
+  },
+  {
+    songId: "SID-120",
+    artistId: "AID-1111",
+    title: "Eurovision",
+    album: "23",
+    year: "2022",
+    duration: 243,
+    url: "https://open.spotify.com/track/4f5TKGvkD4TkTjr61GKNAG?si=0215a114706148b6",
+    explicit: false
+  },
+
+  // Eminem's songs
+  {
+    songId: "SID-201",
+    artistId: "AID-2222",
+    title: "Without Me",
+    album: "The Eminem Show",
+    year: "2002",
+    duration: 290,
+    url: "https://open.spotify.com/track/7lQ8MOhq6IN2w8EYcFNSUk?si=0afa60adbd4b4beb",
+    explicit: false
+  },
+  {
+    songId: "SID-202",
+    artistId: "AID-2222",
+    title: "The Real Slim Shady",
+    album: "The Marshall Mathers LP",
+    year: "2000",
+    duration: 284,
+    url: "https://open.spotify.com/track/3yfqSUWxFvZELEM4PmlwIR?si=c036626bb24d403d",
+    explicit: false
+  },
+  {
+    songId: "SID-203",
+    artistId: "AID-2222",
+    title: "Venom",
+    album: "Kamikaze",
+    year: "2018",
+    duration: 269,
+    url: "https://open.spotify.com/track/3kpYJjvM8Ja6btr5hEJLWc?si=e7e5726192384bcf",
+    explicit: false
+  },
+  {
+    songId: "SID-204",
+    artistId: "AID-2222",
+    title: "Lose Yourself",
+    album: "8 Mile",
+    year: "2002",
+    duration: 326,
+    url: "https://open.spotify.com/track/5Z01UMMf7V1o0MzF86s6WJ?si=9d7bd59bb4aa45cd",
+    explicit: true
+  },
+  {
+    songId: "SID-205",
+    artistId: "AID-2222",
+    title: "Mockingbird",
+    album: "Encore",
+    year: "2004",
+    duration: 250,
+    url: "https://open.spotify.com/track/561jH07mF1jHuk7KlaeF0s?si=4e33b6de9d93463f",
+    explicit: false
+  },
+  {
+    songId: "SID-206",
+    artistId: "AID-2222",
+    title: "Till I Collapse",
+    album: "The Eminem Show",
+    year: "2002",
+    duration: 297,
+    url: "https://open.spotify.com/track/4xkOaSrkexMciUUogZKVTS?si=c71b98155eed4861",
+    explicit: false
+  },
+  {
+    songId: "SID-207",
+    artistId: "AID-2222",
+    title: "Rap God",
+    album: "The Marshall Mathers LP 2",
+    year: "2013",
+    duration: 363,
+    url: "https://open.spotify.com/track/6or1bKJiZ06IlK0vFvY75k?si=d3bdf79c446b4a24",
+    explicit: false
+  },
+  {
+    songId: "SID-208",
+    artistId: "AID-2222",
+    title: "Godzilla",
+    album: "Music to Be Murdered By",
+    year: "2020",
+    duration: 210,
+    url: "https://open.spotify.com/track/7FIWs0pqAYbP91WWM0vlTQ?si=afa43feb39dd43c9",
+    explicit: true
+  },
+  {
+    songId: "SID-209",
+    artistId: "AID-2222",
+    title: "Sing for the Moment",
+    album: "The Eminem Show",
+    year: "2002",
+    duration: 339,
+    url: "https://open.spotify.com/track/3CpoeW0cZSDzIRv5z34F87?si=2e6b08f4fe2446b0",
+    explicit: false
+  },
+  {
+    songId: "SID-210",
+    artistId: "AID-2222",
+    title: "Superman",
+    album: "The Eminem Show",
+    year: "2002",
+    duration: 380,
+    url: "https://open.spotify.com/track/4woTEX1wYOTGDqNXuavlRC?si=2e9e6d6ab58c4239",
+    explicit: false
+  },
+  {
+    songId: "SID-211",
+    artistId: "AID-2222",
+    title: "Tobey",
+    album: "The Death of Slim Shady (Coup de Grâce)",
+    year: "2024",
+    duration: 284,
+    url: "https://open.spotify.com/track/1ymWIr4E5x6xORlDO0bXlP?si=f4154be074b54ead",
+    explicit: false
+  },
+  {
+    songId: "SID-212",
+    artistId: "AID-2222",
+    title: "Stan",
+    album: "The Marshall Mathers LP",
+    year: "2000",
+    duration: 404,
+    url: "https://open.spotify.com/track/3UmaczJpikHgJFyBTAJVoz?si=1d2788f3903a43c4",
+    explicit: false
+  },
+  {
+    songId: "SID-213",
+    artistId: "AID-2222",
+    title: "Big Weenie",
+    album: "Encore",
+    year: "2004",
+    duration: 266,
+    url: "https://open.spotify.com/track/4dh34dyav8M5jCN3yidn8Z?si=059c582b52814547",
+    explicit: false
+  },
+  {
+    songId: "SID-214",
+    artistId: "AID-2222",
+    title: "My Name Is",
+    album: "The Slim Shady LP",
+    year: "1999",
+    duration: 268,
+    url: "https://open.spotify.com/track/0Fv6CfegUbD5Egs0dj8CVB?si=686ea307487a492e",
+    explicit: true
+  },
+  {
+    songId: "SID-215",
+    artistId: "AID-2222",
+    title: "Killshot",
+    album: "Killshot",
+    year: "2018",
+    duration: 253,
+    url: "https://open.spotify.com/track/1jo3TwNGCXfNdB5uba3ZVv?si=aeb05f6916ee443a",
+    explicit: true
+  },
+  {
+    songId: "SID-216",
+    artistId: "AID-2222",
+    title: "The Monster",
+    album: "The Marshall Mathers LP 2",
+    year: "2013",
+    duration: 250,
+    url: "https://open.spotify.com/track/3aUp8U4MNWctRXlY5MPysv?si=a3d76c90985741a4",
+    explicit: false
+  },
+  {
+    songId: "SID-217",
+    artistId: "AID-2222",
+    title: "Bump Heads",
+    album: "The Eminem Show",
+    year: "2002",
+    duration: 281,
+    url: "https://open.spotify.com/track/4OYGHze5MiMkgozardIRxU?si=a253464a1e944ec2",
+    explicit: false
+  },
+  {
+    songId: "SID-218",
+    artistId: "AID-2222",
+    title: "No Love",
+    album: "Recovery",
+    year: "2010",
+    duration: 299,
+    url: "https://open.spotify.com/track/7bHT9osSq1rwT2yaImzqCi?si=d67a50febbed4e1d",
+    explicit: false
+  },
+  {
+    songId: "SID-219",
+    artistId: "AID-2222",
+    title: "Shake That",
+    album: "Curtain Call: The Hits",
+    year: "2005",
+    duration: 274,
+    url: "https://open.spotify.com/track/0fGh0trRN1xz3s41r7V5ce?si=be4b639da3be431b",
+    explicit: false
+  },
+  {
+    songId: "SID-220",
+    artistId: "AID-2222",
+    title: "Ass Like That",
+    album: "Encore",
+    year: "2004",
+    duration: 265,
+    url: "https://open.spotify.com/track/4gXdMZkBN1neE9nX6yRALa?si=77cc6e8aa1ef454f",
+    explicit: false
+  },
+
+  // Chance The Rapper's songs
+  {
+    songId: "SID-301",
+    artistId: "AID-3333",
+    title: "Stars Out",
+    album: "Stars Out",
+    year: "2024",
+    duration: 107,
+    url: "https://open.spotify.com/track/1mTHqHDmUHhAKmO9mRur4z?si=53fa273095574d28",
+    explicit: true
+  },
+  {
+    songId: "SID-302",
+    artistId: "AID-3333",
+    title: "Cocoa Butter Kisses",
+    album: "Acid Rap",
+    year: "2013",
+    duration: 307,
+    url: "https://open.spotify.com/track/0aMHIW1lqrulVCx0LLlr6a?si=c975f739a36649ab",
+    explicit: false
+  },
+  {
+    songId: "SID-303",
+    artistId: "AID-3333",
+    title: "No Problem",
+    album: "Coloring Book",
+    year: "2016",
+    duration: 304,
+    url: "https://open.spotify.com/track/0v9Wz8o0BT8DU38R4ddjeH?si=559092f5ef104b40",
+    explicit: false
+  },
+  {
+    songId: "SID-304",
+    artistId: "AID-3333",
+    title: "Same Drugs",
+    album: "Coloring Book",
+    year: "2016",
+    duration: 257,
+    url: "https://open.spotify.com/track/6m9qPYXmhge2QhBLfFKnVF?si=73f4087865e04537",
+    explicit: false
+  },
+  {
+    songId: "SID-305",
+    artistId: "AID-3333",
+    title: "Slide Around",
+    album: "The Big Day",
+    year: "2019",
+    duration: 270,
+    url: "https://open.spotify.com/track/0QBRshHeQLEThdj2TqxM9n?si=88cde28870084f26",
+    explicit: false
+  },
+  {
+    songId: "SID-306",
+    artistId: "AID-3333",
+    title: "The Highs & The Lows",
+    album: "The Highs & The Lows",
+    year: "2022",
+    duration: 255,
+    url: "https://open.spotify.com/track/6f7bBfMOHM3rjbRryYQaca?si=5516316b41fb454b",
+    explicit: false
+  },
+  {
+    songId: "SID-307",
+    artistId: "AID-3333",
+    title: "Hot Shower",
+    album: "The Big Day",
+    year: "2019",
+    duration: 225,
+    url: "https://open.spotify.com/track/00ko9WaS4jOX1kEk3gvHjf?si=e961d3e988c9442d",
+    explicit: false
+  },
+  {
+    songId: "SID-308",
+    artistId: "AID-3333",
+    title: "Juice",
+    album: "Acid Rap",
+    year: "2013",
+    duration: 215,
+    url: "https://open.spotify.com/track/4J2s7nWDdA7i92sojYX8RJ?si=ae9c3ef7fea644af",
+    explicit: false
+  },
+  {
+    songId: "SID-309",
+    artistId: "AID-3333",
+    title: "Juice",
+    album: "Acid Rap",
+    year: "2013",
+    duration: 215,
+    url: "https://open.spotify.com/track/4J2s7nWDdA7i92sojYX8RJ?si=ae9c3ef7fea644af",
+    explicit: false
+  },
+  {
+    songId: "SID-310",
+    artistId: "AID-3333",
+    title: "Summer Friends",
+    album: "Coloring Book",
+    year: "2016",
+    duration: 290,
+    url: "https://open.spotify.com/track/2fl0B0OaXjWbjHCQFx2O8W?si=3f298cabf6bf4bf5",
+    explicit: false
+  },
+  {
+    songId: "SID-311",
+    artistId: "AID-3333",
+    title: "Favorite Song",
+    album: "Acid Rap",
+    year: "2013",
+    duration: 185,
+    url: "https://open.spotify.com/track/1sFstGV1Z3Aw5TDFCiT7vK?si=05bca668c21c4913",
+    explicit: true
+  },
+  {
+    songId: "SID-312",
+    artistId: "AID-3333",
+    title: "Juke Jam",
+    album: "Coloring Book",
+    year: "2016",
+    duration: 219,
+    url: "https://open.spotify.com/track/3eze1OsZ1rqeXkKStNfTmi?si=bef9dd6006a54dbc",
+    explicit: false
+  },
+  {
+    songId: "SID-313",
+    artistId: "AID-3333",
+    title: "Holy",
+    album: "Justice",
+    year: "2021",
+    duration: 212,
+    url: "https://open.spotify.com/track/5u1n1kITHCxxp8twBcZxWy?si=da79bfe8205d4f66",
+    explicit: true
+  },
+  {
+    songId: "SID-314",
+    artistId: "AID-3333",
+    title: "Acid Rain",
+    album: "Acid Rain",
+    year: "2013",
+    duration: 216,
+    url: "https://open.spotify.com/track/0yLsJKNJHmSnmSGl3ZodYn?si=60699ddf00624ee4",
+    explicit: false
+  },
+  {
+    songId: "SID-315",
+    artistId: "AID-3333",
+    title: "Smoke Again",
+    album: "Acid Rain",
+    year: "2013",
+    duration: 272,
+    url: "https://open.spotify.com/track/0oeqJpFHNRjxa0kOMh4eQY?si=ef5149d3c15d430a",
+    explicit: false
+  },
+  {
+    songId: "SID-316",
+    artistId: "AID-3333",
+    title: "Blessings",
+    album: "Coloring Book",
+    year: "2016",
+    duration: 221,
+    url: "https://open.spotify.com/track/2VQc9orzwE6a5qFfy54P6e?si=304fcf5d5e284e25",
+    explicit: false
+  },
+  {
+    songId: "SID-317",
+    artistId: "AID-3333",
+    title: "Pusha Man",
+    album: "Acid Rap",
+    year: "2013",
+    duration: 139,
+    url: "https://open.spotify.com/track/4ygQUye7obtqhSltSxp18H?si=be227fc282034833",
+    explicit: false
+  },
+  {
+    songId: "SID-318",
+    artistId: "AID-3333",
+    title: "Buried Alive",
+    album: "Buried Alive",
+    year: "2024",
+    duration: 122,
+    url: "https://open.spotify.com/track/17hgE2h9PhLC6RJuFm2BEQ?si=c382b7dde46c4c13",
+    explicit: true
+  },
+  {
+    songId: "SID-319",
+    artistId: "AID-3333",
+    title: "Paranoia",
+    album: "Acid Rap",
+    year: "2013",
+    duration: 275,
+    url: "https://open.spotify.com/track/6Oe5tmI1bfguCp6dKHbSj6?si=c6e24f13eb56494f",
+    explicit: false
+  },
+  {
+    songId: "SID-320",
+    artistId: "AID-3333",
+    title: "Lost",
+    album: "Acid Rap",
+    year: "2013",
+    duration: 184,
+    url: "https://open.spotify.com/track/5v2ePUTZiv1mPDCD1nSbfF?si=26a3033f756b46d6",
+    explicit: false
+  }
+];
